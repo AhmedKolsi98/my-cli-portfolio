bbba49add9eb4f6755396c5e02816f47ff075d8a
AhmedKolsi98 <47602186+AhmedKolsi98@users.noreply.github.com>
2024-10-30 23:55:15 +0100

    update info on several files

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -8,11 +8,10 @@ const About: React.FC = () => {
   return (
     <AboutWrapper data-testid="about">
       <p>
-        Hi, my name is <HighlightSpan>Sat Naing</HighlightSpan>!
+        Hi, my name is <HighlightSpan>Ahmed Kolsi</HighlightSpan>!
       </p>
       <p>
-        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Yangon,
-        Myanmar.
+        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Tunis, Tunisia.
       </p>
       <p>
         I am passionate about writing codes and <br />
